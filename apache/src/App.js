2f4215bc6d46f66f2601181af19ef1283b84c3ae
Ridham <ridhampatel2002@gmail.com>
2024-03-14 16:29:27 +0530

    Complete

---
+++ apache/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello <code>Ridham,</code> piple chal gai.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Success
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
